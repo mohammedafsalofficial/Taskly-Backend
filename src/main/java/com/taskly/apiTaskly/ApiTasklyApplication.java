55175352ebf5eececb871847ccef8c80355716fe
Mohammed Afsal <mohammedafsaltut@gmail.com>
2025-04-05 05:25:08 +0530

    initial commit

---
+++ src/main/java/com/taskly/apiTaskly/ApiTasklyApplication.java
@@ -0,0 +1,13 @@
+package com.taskly.apiTaskly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTasklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTasklyApplication.class, args);
+	}
+
+}
