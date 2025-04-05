efde5851f0f464c4c5eef2896bcd0b96751bbe47
Mohammed Afsal <mohammedafsaltut@gmail.com>
2025-04-05 06:25:22 +0530

    Basic security config setup

---
+++ src/main/java/com/taskly/apiTaskly/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.taskly.apiTaskly.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((request) ->
+                request.requestMatchers("/api/register")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
